b16e8dd2de2f7d26bfa24e42109ff20b8f9e45a8
Dominique <dominique@chez-dominique.fr>
2016-12-07 08:15:19 +0100

    (refactoring):refactoring

--- classes/MypdfClass.php
+++ classes/MypdfClass.php
@@ -18,9 +18,9 @@
  * versions in the future. If you wish to customize PrestaShop for your
  * needs please refer to http://www.prestashop.com for more information.
  *
- * @author  Dominique <dominique@chez-dominique.fr>
- * @copyright   2007-2016 Chez-dominique
- */
+ * @author    Dominique <dominique@chez-dominique.fr>
+ * @copyright 2007-2016 Chez-dominique
+ **/
 
 if (!defined('_PS_VERSION_')) {
     exit;
@@ -38,7 +38,6 @@ class MypdfClass extends TCPDF
 // Set font
         $this->SetFont('helvetica', 'I', 8);
 // Page number
-        $this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . '/' . $this->getAliasNbPages(), 0, false, 'C', 0, '', 0,
-            false, 'T', 'M');
+        $this->Cell(0, 10, 'Page ' . $this->getAliasNumPage() . '/' . $this->getAliasNbPages(), 0, false, 'C', 0, '', 0, false, 'T', 'M');
     }
 }
